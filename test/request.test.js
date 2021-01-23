789bdf49ecbaab33f0a3442ec6fb8d4c79f78d7a
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-23 06:08:37 -0300

    add description of Request tests

--- test/request.test.js
+++ test/request.test.js
@@ -1,8 +1,23 @@
 const assert = require('assert');
-const { describe, it } = require('mocha');
+const { describe, it, before, afterEach } = require('mocha');
+const { createSandbox } = require('sinon');
 
-describe('#test', () => {
-  it('should test', () => {
-    assert.ok(true);
+const Request = require('../src/request');
+
+describe('Request helpers', () => {
+  const timeout = 15;
+  let sandbox, request;
+
+  before(() => {
+    sandbox = createSandbox();
+    request = new Request();
   });
+
+  afterEach(() => sandbox.restore());
+
+  it(`should throw a timeout error when the function has spent more than ${timeout}ms`);
+
+  it('should return ok when promise time is ok');
+
+  it('should return a JSON object after a request');
 });
